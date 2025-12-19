9134aff7cac90b34e1f94f567771e37fce5f2b77
Koushik-KS <kawshikks8@gmail.com>
2025-12-19 23:54:02 +0530

    k

--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -3,10 +3,10 @@ import { FaAngleDown } from "react-icons/fa6";
 
 const CountryDropdown = () => {
   return (
-    <Button className="CountryDrop">
+    <Button className="countryDrop">
       <div className="info d-flex flex-column">
-        <span>Your Location</span>
-        <span>India</span>
+        <span className="label">Your Location</span>
+        <span className="name">India</span>
       </div>
       <span className="ml-auto">
         <FaAngleDown />
