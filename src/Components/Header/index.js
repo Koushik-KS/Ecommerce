389ae224d0ed6c15b56b903ac6106e7ede7008f4
Koushik-KS <kawshikks8@gmail.com>
2025-12-20 00:35:16 +0530

    commit

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/eshop.png';
 import Button from "@mui/material/Button";
 import CountryDropdown  from '../CountryDropdown';
+import { FaSearch } from "react-icons/fa";
 
 const Header=()=>{
     return(
@@ -12,7 +13,7 @@ const Header=()=>{
                 <p className="mb-0 mt-0" >Due to  the <b> ONTIME </b> Delivery</p>
             </div>
         </div>
-        <div className="header">
+        < header className="header">
             <div className="container">
                 <div className="row">
                 <div className="logoWrapper d-flex align-items-center col-sm-2">
@@ -21,13 +22,27 @@ const Header=()=>{
                     </div>
                     <div className='col-sm-10   d-flex align-items-center part2'>
                         <CountryDropdown/>
+
+                    {/* Header search starts here */}
+                    <div className="headerSearch ml-3 mr-3">
+                        <input type='text'/>
+                      <Button>  <FaSearch/></Button>
+                        </div>
+
+
+                    {/* header seach end */}
+
+
+
+
                     </div>
 
 
                     
                 </div>
             </div>  
-        </div>
+        
+        </header>
         </div>
     )
             
