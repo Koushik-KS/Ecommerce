dbd4aa6644c70c1c6962562591a561d9a96849c3
Koushik-KS <kawshikks8@gmail.com>
2025-12-22 00:48:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import Home from './Pages/Home';
-import Header from './Components/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Header from "./Components/Header";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
+const MyContext = createContext();
 
 function App() {
+  const [countryList, setCountryList] = useState([]);
+
+  useEffect(() => {
+    getCountry("https://countriesnow.space/api/v0.1/countries/");
+  }, []);
+
+  const getCountry = async (url) => {
+    try {
+      const res = await axios.get(url);
+      setCountryList(res.data.data);
+      console.log(res.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const values = {
+    countryList,
+  };
+
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" exact={true} element={<Home/>}/>
-    </Routes>
+      <MyContext.Provider value={values}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MyContext.Provider>
     </BrowserRouter>
-    
-  
-      
   );
 }
 
 export default App;
+export { MyContext };
--- src/Components/CountryDropdown/index.js
+++ src/Components/CountryDropdown/index.js
@@ -1,11 +1,11 @@
- import React from "react";
- import { useState } from "react";
+import React, { useState, useContext } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
+import Slide from "@mui/material/Slide";
 import { FaAngleDown } from "react-icons/fa6";
 import { FaSearch } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
-import Slide from '@mui/material/Slide';
+import { MyContext } from "../../App";   // ✅ correct path
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -13,11 +13,11 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 const CountryDropdown = () => {
   const [isOpenModal, setIsOpenModal] = useState(false);
- 
+  const context = useContext(MyContext);
 
   return (
     <>
-      <Button className="countryDrop" onClick={()=>setIsOpenModal(true)}>
+      <Button className="countryDrop" onClick={() => setIsOpenModal(true)}>
         <div className="info d-flex flex-column">
           <span className="label">Your Location</span>
           <span className="name">India</span>
@@ -27,46 +27,37 @@ const CountryDropdown = () => {
         </span>
       </Button>
 
-      <Dialog open={isOpenModal} onClick={()=>setIsOpenModal(false)} className="locationModal " TransitionComponent={Transition}>
-        
-          <h4 className=" mb-0">Choose your Delivery Location</h4>
+      <Dialog
+        open={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+        className="locationModal"
+        TransitionComponent={Transition}
+      >
+        <h4 className="mb-0">Choose your Delivery Location</h4>
         <p>Enter your address and we will specify the offer for your area.</p>
-       <Button className="close_" onClick={()=>setIsOpenModal(false)}><IoClose /></Button> 
 
-        <div className="headerSearch   w-100">
-                        <input type='text' placeholder='Search  your area' />
-                      <Button>  <FaSearch/></Button>
-                        </div>
+        <Button className="close_" onClick={() => setIsOpenModal(false)}>
+          <IoClose />
+        </Button>
 
-                        <ul className="countryList mt-3">
-                          <li><Button  onClick={()=>setIsOpenModal(false)}>India</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>USA</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Russia</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Canada</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Japan</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>south Africa</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>England</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Australia</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Sri Lank</Button></li>   
-                             <li><Button  onClick={()=>setIsOpenModal(false)}>Iran</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Isrel</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Chaina</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Tailland</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Germany</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Nepal</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>France</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Spain</Button></li>  
+        <div className="headerSearch w-100">
+          <input type="text" placeholder="Search your area" />
+          <Button>
+            <FaSearch />
+          </Button>
+        </div>
 
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>France</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Spain</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Singapore</Button></li>
-                           <li><Button  onClick={()=>setIsOpenModal(false)}>Mexico</Button></li>
-                            <li><Button  onClick={()=>setIsOpenModal(false)}>Saudi Arabia</Button></li>
-                           
-                        </ul>
-
+        <ul className="countryList mt-3">
+          {context.countryList?.length !== 0 &&
+            context.countryList.map((item, index) => (
+              <li key={index}>
+                <Button onClick={() => setIsOpenModal(false)}>
+                  {item.country}
+                </Button>
+              </li>
+            ))}
+        </ul>
       </Dialog>
-
     </>
   );
 };
