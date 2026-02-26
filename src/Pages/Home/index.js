f8a8c3a382f1d2b0ffa0ca6ce29bcd1ff6f94932
Koushik-KS <kawshikks8@gmail.com>
2026-02-26 22:25:19 +0530

    commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,12 +1,30 @@
+
 import HomeBanner from "../../Components/HomeBanner";
 
-const Home=()=>{
-    return(
-        
-      <>
-      <HomeBanner/>
-      </>
-    )
-            
-}
+const Home = () => {
+  return (
+    <>
+      <HomeBanner />
+
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="banner">
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeuNb8OMqMpAfkftf2BvE5i2vKM5GdO-LebA&s"
+                  alt="Fashion banner"
+                  className="cursor"
+                />
+              </div>
+            </div>
+
+            <div className="col-md-3"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Home;
