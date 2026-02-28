315305bc0f073a22f2758a20883aa37b56db2f76
Koushik-KS <kawshikks8@gmail.com>
2026-03-01 00:47:42 +0530

    commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -8,6 +8,7 @@ import Rating from '@mui/material/Rating';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ProductItem from "../../Components/ProductItem";
 
 /* Custom Previous Arrow */
 const PrevArrow = ({ onClick }) => {
@@ -74,96 +75,28 @@ const Home = () => {
               </div>
 
               <div className="product_row w-100">
-                <Slider {...productSliderOptions}>
-
-                  <div className="item productItem">
-                    <div className="imgWrapper">
-                      <img 
-                        src="https://m.media-amazon.com/images/I/71386F6rF+L._AC_UY327_FMwebp_QL65_.jpg" 
-                        alt="product1"
-                        className="w-100"/>
-                    </div>
-                    <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju | Firm Texture, Rich Taste | Healthy Snack</h4>
-                   <span className="text-success d-block">In Stock</span>
-                    <Rating className="mt-2 mb-2" name="read-only" value={5} readOnly size="small"precision={0.5}/>
-                      <div className="d-flex">
-                        <span className="oldPrice">₹350</span>
-                         <span className="netPrice text-danger ml-3">₹299</span>
-                      </div>
-                 
-                  </div>
- 
-
-                  <div className="item productItem">
-                    <div className="imgWrapper">
-                      <img 
-                        src="https://m.media-amazon.com/images/I/71sXG9x1bSL._AC_UY327_FMwebp_QL65_.jpg" 
-                        alt="product2"
-                        className="w-100"
-                      />
-                    </div>
-                     <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju | Firm Texture, Rich Taste | Healthy Snack</h4>
-                   <span className="text-success d-block">In Stock</span>
-                    <Rating className="mt-2 mb-2" name="read-only" value={5} readOnly size="small"precision={0.5}/>
-                      <div className="d-flex">
-                        <span className="oldPrice">₹350</span>
-                         <span className="netPrice text-danger ml-3">₹299</span>
-                      </div>
-                  </div>
-
-                  <div className="item productItem">
-                    <div className="imgWrapper">
-                      <img 
-                        src="https://m.media-amazon.com/images/I/81u0Uo4nQdL._AC_UY327_FMwebp_QL65_.jpg" 
-                        alt="product3"
-                        className="w-100"
-                      />
-                    </div>
-                     <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju | Firm Texture, Rich Taste | Healthy Snack</h4>
-                   <span className="text-success d-block">In Stock</span>
-                    <Rating className="mt-2 mb-2" name="read-only" value={5} readOnly size="small"precision={0.5}/>
-                      <div className="d-flex">
-                        <span className="oldPrice">₹350</span>
-                         <span className="netPrice text-danger ml-3">₹299</span>
-                      </div>
-                  </div>
-
-                  <div className="item productItem">
-                    <div className="imgWrapper">
-                      <img 
-                        src="https://m.media-amazon.com/images/I/71AHZUc7CFL._AC_UY327_FMwebp_QL65_.jpg" 
-                        alt="product4"
-                        className="w-100"
-                      />
-                    </div>
-                     <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju | Firm Texture, Rich Taste | Healthy Snack</h4>
-                   <span className="text-success d-block">In Stock</span>
-                    <Rating className="mt-2 mb-2" name="read-only" value={5} readOnly size="small"precision={0.5}/>
-                      <div className="d-flex">
-                        <span className="oldPrice">₹350</span>
-                         <span className="netPrice text-danger ml-3">₹299</span>
-                      </div>
-                  </div>
-
-                  <div className="item productItem">
-                    <div className="imgWrapper">
-                      <img 
-                        src="https://m.media-amazon.com/images/I/71D96ykEFNL._AC_UY327_FMwebp_QL65_.jpg" 
-                        alt="product5"
-                        className="w-100"
-                      />
-                    </div>
-                     <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju | Firm Texture, Rich Taste | Healthy Snack</h4>
-                   <span className="text-success d-block">In Stock</span>
-                    <Rating className="mt-2 mb-2" name="read-only" value={5} readOnly size="small"precision={0.5}/>
-                      <div className="d-flex">
-                        <span className="oldPrice">₹350</span>
-                         <span className="netPrice text-danger ml-3">₹299</span>
-                      </div>
-                  </div>
- 
-                </Slider>
+                <ProductItem />
               </div>
+
+
+              <div className="d-flex align-items-center justify-content-between mb-2 w-100">
+                <div className="info">
+                  <h3 className="mb-0 hd">NEW PRODUCTS</h3>
+                  <p className="text-light text-sml mb-0">New product with updated  stocks.
+                    
+                  </p>
+                </div>
+
+                <Button className="viewAllBtn">
+                  View All <IoIosArrowRoundForward />
+                </Button>
+              </div>
+
+              <div className="product_row w-100">
+                <ProductItem />
+              </div>
+
+
 
             </div>
           </div>
