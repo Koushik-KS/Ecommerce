7690145a32a01dc4b54e6b969e6ee0ea20116c8e
Koushik-KS <kawshikks8@gmail.com>
2026-03-03 00:04:49 +0530

    commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -6,6 +6,9 @@ import Button from "@mui/material/Button";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import ProductItem from "../../Components/ProductItem";
 import HomeCat from "../../Components/HomeCat";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation } from "swiper/modules";
 
 const Home = () => {
   return (
@@ -18,7 +21,7 @@ const Home = () => {
         <div className="container">
           <div className="row">
 
-            {/* LEFT BANNERS */}
+            
             <div className="col-md-3">
               <div className="banner">
                 <img
@@ -27,7 +30,7 @@ const Home = () => {
                   className="cursor w-100"
                 />
               </div>
-              <div className="banner mt-3">
+              <div className="banner mt-4">
                 <img
                   src={banner2}
                   alt="Fashion banner"
@@ -36,12 +39,13 @@ const Home = () => {
               </div>
             </div>
 
-            {/* RIGHT PRODUCTS */}
+          
             <div className="col-md-9 productRow">
 
               {/* BEST SELLER */}
-              <div className="d-flex align-items-center justify-content-between mb-2 w-100">
-                <div className="info">
+              <div className="d-flex align-items-center ">
+             
+                <div className="info w-75">
                   <h3 className="mb-0 hd">BEST SELLER</h3>
                   <p className="text-light text-sml mb-0">
                     Do not miss the current offers until the end of the month.
@@ -52,12 +56,44 @@ const Home = () => {
                   View All <IoIosArrowRoundForward />
                 </Button>
               </div>
-
-              {/* ProductItem is already a Swiper → DON'T WRAP */}
+              <div className="product_row w-100 ">
+                 <Swiper 
+            slidesPerView={4}
+            spaceBetween={0}
+            navigation={true}
+            slidesPerGroup={3}
+            modules={[Navigation]}
+            className="mySwiper">
+              <SwiperSlide>
               <ProductItem />
+              </SwiperSlide>
 
-              {/* NEW PRODUCTS */}
-              <div className="d-flex align-items-center justify-content-between mb-2 w-100 mt-5">
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+              
+               </Swiper>
+               
+
+</div>
+
+
+            
+
+             
+              <div className="d-flex align-items-center ">
                 <div className="info">
                   <h3 className="mb-0 hd">NEW PRODUCTS</h3>
                   <p className="text-light text-sml mb-0">
@@ -70,10 +106,38 @@ const Home = () => {
                 </Button>
               </div>
 
-              
+              <div className="product_row w-100 ">
+                 <Swiper 
+            slidesPerView={4}
+            spaceBetween={0}
+            navigation={true}
+            slidesPerGroup={3}
+            modules={[Navigation]}
+            className="mySwiper">
+              <SwiperSlide>
               <ProductItem />
-            </div>
+              </SwiperSlide>
 
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+
+               <SwiperSlide>
+              <ProductItem />
+              </SwiperSlide>
+              
+               </Swiper>
+               
+          </div>
+</div>
           </div>
         </div>
       </section>
