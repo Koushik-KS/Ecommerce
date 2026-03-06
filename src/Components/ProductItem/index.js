9dfa300726ff99634b41039b7536adf7d78e1a5d
Koushik-KS <kawshikks8@gmail.com>
2026-03-06 23:33:06 +0530

    commit

--- src/Components/ProductItem/index.js
+++ src/Components/ProductItem/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -11,60 +11,71 @@ import { Navigation } from "swiper/modules";
 import ProductModal from "../ProductModal";
 
 const ProductItem = () => {
+
+  const [isOpenProductModal, setisOpenProductModal] = useState(false);
+
+  const viewProductDetails = (id) => {
+    setisOpenProductModal(true);
+  };
+
+  const closeProductModal = () => {
+    setisOpenProductModal(false);
+  };
+
   return (
     <>
-    <div className="item productItem">
-      
-      <div className="imgWrapper">
+      <div className="item productItem">
+        <div className="imgWrapper">
 
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={0}
-          navigation={true}
-          modules={[Navigation]}
-           slidesPerGroup={1}
-          className="mySwiper"
-        >
-          
-          <SwiperSlide>
-            <img
-              src="https://m.media-amazon.com/images/I/71386F6rF+L._AC_UY327_FMwebp_QL65_.jpg"
-              alt="product1"
-              className="w-100"
-            />
-          </SwiperSlide>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={0}
+            navigation={true}
+            modules={[Navigation]}
+            slidesPerGroup={1}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img
+                src="https://m.media-amazon.com/images/I/71386F6rF+L._AC_UY327_FMwebp_QL65_.jpg"
+                alt="product1"
+                className="w-100"
+              />
+            </SwiperSlide>
 
-         
+          </Swiper>
 
-          
+          <span className="badge badge-primary">28%</span>
 
-       </Swiper>
-       
+          <div className="actions">
+            <Button onClick={() => viewProductDetails(1)}>
+              <TfiFullscreen />
+            </Button>
 
-        <span className="badge badge-primary">28%</span>
+            <Button>
+              <CiHeart />
+            </Button>
+          </div>
 
-        <div className="actions">
-          <Button><TfiFullscreen /></Button>
-          <Button><CiHeart /></Button>
+        </div>
+
+        <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju</h4>
+
+        <span className="text-success d-block">In Stock</span>
+
+        <Rating value={5} readOnly size="small" precision={0.5} />
+
+        <div className="d-flex">
+          <span className="oldPrice">₹350</span>
+          <span className="netPrice text-danger ml-3">₹299</span>
         </div>
 
       </div>
 
-      <h4>Vedaka Whole Cashews | 200 Gram | Grade W320 Kaju</h4>
+      {isOpenProductModal === true && (
+        <ProductModal closeProductModal={closeProductModal} />
+      )}
 
-      <span className="text-success d-block">In Stock</span>
-
-      <Rating value={5} readOnly size="small"   precision={0.5}/>
-    
-
-      <div className="d-flex">
-        <span className="oldPrice">₹350</span>
-        <span className="netPrice text-danger ml-3">₹299</span>
-     
-      
-</div>
-</div>
-  
     </>
   );
 };
--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -3,14 +3,12 @@ import Button from "@mui/material/Button";
 import { IoClose } from "react-icons/io5";
 
 
-const ProductModal  = ()=> {
+const ProductModal  = (props)=> {
     return(
         <>
-        <Dialog open={true} >
-      
-       
-
- <Button className='close_'><IoClose /> </Button>
+        <Dialog open={true} onClose={()=>props.closeProductModal()}> 
+              <Button className='close_'><IoClose /> </Button>
+              <h4 class="mb-0">All Natural Italian-Style Chicken Meatballs</h4>
 
        
           
