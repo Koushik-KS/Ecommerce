9a8d5209c49261b315dc0cbbe35991746e4b77d1
Koushik-KS <kawshikks8@gmail.com>
2026-03-07 20:50:19 +0530

    commit

--- src/Components/ProductModal/index.js
+++ src/Components/ProductModal/index.js
@@ -1,37 +1,115 @@
+import React, { useRef } from "react";
 import Dialog from "@mui/material/Dialog";
 import Button from "@mui/material/Button";
 import { IoClose } from "react-icons/io5";
-import Rating from '@mui/material/Rating';
+import Rating from "@mui/material/Rating";
+import Slider from "react-slick";
+import InnerImageZoom from "react-inner-image-zoom";
 
-const ProductModal  = (props)=> {
-    return(
-        <>
-        <Dialog open={true} className="productModal" onClose={()=>props.closeProductModal()}> 
-              <Button className='close_' onClick={()=>props.closeProductModal()}><IoClose /> </Button>
-              <h4 class="mb-1 font-weight-bold">All Natural Italian-Style Chicken Meatballs</h4>
-              <div className="d-flex align-items-center">
-                <div className="d-flex align-items-center mr-4">
+import "react-inner-image-zoom/lib/styles.min.css";
 
-                <span>Brands:</span>
-                  <span className="ml-2">,<b>Welch's</b></span>
-                  </div>
-                   <Rating name="read-only" value={5} size="small" precision={0.5} read-only/>
 
-              </div>
+const ProductModal = (props) => {
 
+  const zoomSliderBig = useRef();
+  const zoomSlider = useRef();
+
+  var settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    fade: false,
+    arrows:true
+  };
+
+  var settings2 = {
+    dots: false,
+    infinite: false,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: false,
+    arrows: false,
+  };
+
+  return (
+    <>
+      <Dialog open={true} className="productModal" onClose={() => props.closeProductModal()}>
+
+        <Button className="close_" onClick={() => props.closeProductModal()}>
+          <IoClose />
+        </Button>
+
+        <h4 className="mb-1 font-weight-bold">
+          All Natural Italian-Style Chicken Meatballs
+        </h4>
+
+        <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center mr-4">
+            <span>Brands:</span>
+            <span className="ml-2"><b>Welch's</b></span>
+          </div>
+
+          <Rating name="read-only" value={5} size="small" precision={0.5} readOnly />
+        </div>
+
+        <hr />
+
+        <div className="row mt-2 productDetaileModal">
+          <div className="col-md-5">
+            <div className="productZoom">
+
+              <Slider {...settings2} className="zoomSliderBig" ref={zoomSliderBig}>
+                <div className="item">
+                  <InnerImageZoom
+                    zoomType="hover"
+                    zoomScale={1}
+                    src="https://m.media-amazon.com/images/I/818LBp+THNL._SX679_.jpg"
+                  />
+                </div>
+              </Slider>
+
+            </div>
+
+
+        <Slider {...settings} className="zoomSlider" ref={zoomSlider}>
+        <div className="item" >
+        <img src="https://m.media-amazon.com/images/I/818LBp+THNL._SX679_.jpg" className="w-100" />
+        </div>
+    
+  <div className="item" >
+        <img src="https://m.media-amazon.com/images/I/818LBp+THNL._SX679_.jpg" className="w-100" />
+        </div>
        
-          
-</Dialog>
-        
-        
-        
-        
-        </>
+
+        <div className="item" >
+        <img src="https://m.media-amazon.com/images/I/818LBp+THNL._SX679_.jpg" className="w-100" />
+        </div>
+   
+     <div className="item" >
+        <img src="https://m.media-amazon.com/images/I/818LBp+THNL._SX679_.jpg" className="w-100" />
+        </div>
+       
+        <div className="item" >
+        <img src="https://m.media-amazon.com/images/I/818LBp+THNL._SX679_.jpg" className="w-100" />
+        </div>
+       </Slider>
+       
 
 
+</div>
 
- )
+          <div className="col-md-7">
+          </div>
+        </div>
 
+        <br /><br /><br />
 
-}
+      </Dialog>
+    </>
+  );
+};
+
 export default ProductModal;
