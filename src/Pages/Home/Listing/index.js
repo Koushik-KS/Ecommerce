63059efbeb4084c08ce8934839a73801c7550742
Koushik-KS <kawshikks8@gmail.com>
2026-03-10 23:54:26 +0530

    commit

--- src/Pages/Home/Listing/index.js
+++ src/Pages/Home/Listing/index.js
@@ -8,7 +8,8 @@ const Listing=()=>{
             <div className="productListing d-flex">
                 <Sidebar/>
                 <div className="content_right">
-                    content_right
+                   <img src="https://i.pinimg.com/1200x/05/0f/fa/050ffa755bd75b8666c69c16b69550f9.jpg"
+                   className="w-100" style={{borderRadius:'8px'}}/>
 
                 </div>
 
