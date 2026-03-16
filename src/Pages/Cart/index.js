07354c5a0b6f9c79a35911e7c5ccc87fd8435783
Koushik-KS <kawshikks8@gmail.com>
2026-03-16 22:26:26 +0530

    commit

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -1,32 +1,36 @@
 import { Link } from "react-router-dom";
 import Rating from '@mui/material/Rating';
+import QuantityBox from "../../Components/QuantityBox";
+
+import { IoClose } from "react-icons/io5";
 
 const Cart =() =>{
 
     return(
         <>
-        <section className="sectio cartPage">
+        <section className="section cartPage">
             <div className="container">
+                 <h2 className="hd mb-0">Your Cart</h2>
+                        <p>There are <b>3</b> products in your cart</p>
+
 
                 <div className="row">
                     <div className="col-md-8">
-                        <h2 className="hd mb-0">Your Cart</h2>
-                        <p>There are <b>3</b> products in your cart</p>
-
+                       
                         <div className="table-responsive">
-                    <table className="table">
+                    <table className="table  ">
                         <thead>
                             <tr>
-                            <th>Product</th>
-                            <th>Unit Price</th>
-                            <th>Quantity</th>
-                            <th>Subtotal</th>
-                             <th>Remove</th>
+                            <th width="35%" >Product</th>
+                            <th width="15%">Unit Price</th>
+                            <th width="25%">Quantity</th>
+                            <th width="15%">Subtotal</th>
+                             <th width="10%">Remove</th>
                         </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>
+                                <td width="35%">
                                     <Link to="/product/1">
                                     <div className="d-flex align-items-center cartItemimgWrapper">
                                         <div className="imgWrapper">
@@ -41,10 +45,120 @@ const Cart =() =>{
                                     </div>
                                     </Link>
                                 </td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <td width="15%">₹150</td>
+                                <td width="25%"><QuantityBox/></td>
+                                <td width="15%">₹150</td>
+                                <td width="10%"><span className="remove"><IoClose /></span></td>
+                            </tr>
+
+                             <tr>
+                                <td width="35%">
+                                    <Link to="/product/1">
+                                    <div className="d-flex align-items-center cartItemimgWrapper">
+                                        <div className="imgWrapper">
+                                            <img src="https://i.pinimg.com/736x/ef/1a/fb/ef1afbc7cd59bf7918e1c3d600f53986.jpg" className="w-100"/>
+                                            
+                                       </div>
+
+                                       <div className="info px-3">
+                                        <h6> Icecream</h6> <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+
+                                       </div>
+                                    </div>
+                                    </Link>
+                                </td>
+                                <td width="15%">₹150</td>
+                                <td width="25%"><QuantityBox/></td>
+                                <td width="15%">₹150</td>
+                                <td width="10%"><span className="remove"><IoClose /></span></td>
+                            </tr>
+
+                             <tr>
+                                <td width="35%">
+                                    <Link to="/product/1">
+                                    <div className="d-flex align-items-center cartItemimgWrapper">
+                                        <div className="imgWrapper">
+                                            <img src="https://i.pinimg.com/736x/ef/1a/fb/ef1afbc7cd59bf7918e1c3d600f53986.jpg" className="w-100"/>
+                                            
+                                       </div>
+
+                                       <div className="info px-3">
+                                        <h6> Icecream</h6> <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+
+                                       </div>
+                                    </div>
+                                    </Link>
+                                </td>
+                                <td width="15%">₹150</td>
+                                <td width="25%"><QuantityBox/></td>
+                                <td width="15%">₹150</td>
+                                <td width="10%"><span className="remove"><IoClose /></span></td>
+                            </tr>
+
+                             <tr>
+                                <td width="35%">
+                                    <Link to="/product/1">
+                                    <div className="d-flex align-items-center cartItemimgWrapper">
+                                        <div className="imgWrapper">
+                                            <img src="https://i.pinimg.com/736x/ef/1a/fb/ef1afbc7cd59bf7918e1c3d600f53986.jpg" className="w-100"/>
+                                            
+                                       </div>
+
+                                       <div className="info px-3">
+                                        <h6> Icecream</h6> <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+
+                                       </div>
+                                    </div>
+                                    </Link>
+                                </td>
+                                <td width="15%">₹150</td>
+                                <td width="25%"><QuantityBox/></td>
+                                <td width="15%">₹150</td>
+                                <td width="10%"><span className="remove"><IoClose /></span></td>
+                            </tr>
+
+                             <tr>
+                                <td width="35%">
+                                    <Link to="/product/1">
+                                    <div className="d-flex align-items-center cartItemimgWrapper">
+                                        <div className="imgWrapper">
+                                            <img src="https://i.pinimg.com/736x/ef/1a/fb/ef1afbc7cd59bf7918e1c3d600f53986.jpg" className="w-100"/>
+                                            
+                                       </div>
+
+                                       <div className="info px-3">
+                                        <h6> Icecream</h6> <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+
+                                       </div>
+                                    </div>
+                                    </Link>
+                                </td>
+                                <td width="15%">₹150</td>
+                                <td width="25%"><QuantityBox/></td>
+                                <td width="15%">₹150</td>
+                                <td width="10%"><span className="remove"><IoClose /></span></td>
+                            </tr>
+
+                             <tr>
+                                <td width="35%">
+                                    <Link to="/product/1">
+                                    <div className="d-flex align-items-center cartItemimgWrapper">
+                                        <div className="imgWrapper">
+                                            <img src="https://i.pinimg.com/736x/ef/1a/fb/ef1afbc7cd59bf7918e1c3d600f53986.jpg" className="w-100"/>
+                                            
+                                       </div>
+
+                                       <div className="info px-3">
+                                        <h6> Icecream</h6> <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+
+                                       </div>
+                                    </div>
+                                    </Link>
+                                </td>
+                                <td width="15%">₹150</td>
+                                <td width="25%"><QuantityBox/></td>
+                                <td width="15%">₹150</td>
+                                <td width="10%"><span className="remove"><IoClose /></span></td>
                             </tr>
                         </tbody>
 
@@ -54,6 +168,30 @@ const Cart =() =>{
 
                         </div>
                         <div className="col-md-4">
+                            <div className="card border p-3 cartDetails">
+                                <h4>CART TOTALS</h4>
+
+                                <div className="d-flex align-items-center mb-3">
+                                    <span>Subtotal</span>
+                                    <span className="ml-auto text-red">₹1200</span>
+                                </div>
+
+                                 <div className="d-flex align-items-center mb-3">
+                                    <span>Shipping</span>
+                                    <span className="ml-auto text-red">Free</span>
+                                </div>
+
+                                 <div className="d-flex align-items-center mb-3">
+                                    <span>Estimate For</span>
+                                    <span className="ml-auto text-red">India</span>
+                                </div>
+
+                                 <div className="d-flex align-items-center mb-3">
+                                    <span>Total</span>
+                                    <span className="ml-auto text-red">₹1200</span>
+                                </div>
+
+                            </div>
 
                         </div>
                         </div>                
