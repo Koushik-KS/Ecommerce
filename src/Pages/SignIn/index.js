d12e890423d3362c9052e96e8430146044adebb3
Koushik-KS <kawshikks8@gmail.com>
2026-03-23 20:23:15 +0530

    commiit

--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -1,6 +1,12 @@
 import { useEffect, useContext } from "react";
 import { MyContext } from "../../App";
 import Logo from "../../assets/images/eshop.png";
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import { Link } from "react-router-dom";
+import { FaFacebookF } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaInstagram } from "react-icons/fa";
 const SignIn = () => {
 
     const context = useContext(MyContext);
@@ -33,7 +39,49 @@ const SignIn = () => {
                     <div className="text-center">
                         <img src={Logo}/>
                     </div>
-                    <h2>Sign In</h2>
+                   
+
+                    <form className="mt-3">
+                         <h2>Sign In</h2>
+                        <div className="form-group">
+                           <TextField id="standard-basic" label="Email"type="email" required variant="standard" className="w-100" />
+                        </div>
+
+                         <div className="form-group">
+                           <TextField id="standard-basic" label="Password"type="password" required variant="standard" className="w-100" />
+                        </div>
+
+
+                        <a className="border-effect cursor" >Forget Password?</a>
+
+                        <Button className="btn-blue btn-lg btn-big w-100 mt-3 mb-3">Sign In</Button>
+                        <p>Not Registered? <Link to="/signUp" className="border-effect">Sign UP </Link></p>
+                   
+                        <h6 className="mt-3 text-center font-weight-bold">Or continue with social account</h6>
+
+
+                         <ul className="list list-inline mt-3  mb-1 socials text-center">
+                                                <li className="list-inline-item">
+                                                    <Link to="#"><FaFacebookF /></Link>
+                                                </li>
+                        
+                                                <li className="list-inline-item">
+                                                    <Link to="#"><RiTwitterXFill /></Link>
+                                                </li>
+                        
+                                                <li className="list-inline-item">
+                                                    <Link to="#"><FaInstagram /></Link>
+                                                </li>
+                        
+                                            </ul>
+                                        
+                        
+                   
+                   </form>
+
+
+
+
                 </div>
             </div>
         </section>
