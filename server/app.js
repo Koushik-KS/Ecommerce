9d446a55bc31b6fa879b92f28062b3565a0bbdfc
Koushik-KS <kawshikks8@gmail.com>
2026-03-26 00:07:53 +0530

    commit

--- server/app.js
+++ server/app.js
@@ -1,19 +1,27 @@
-
-const  express = require('express');
+const express = require('express');
 const app = express();
-const bodyParsel=require('body-parser');
-const mongoose =require('mongoose');
-const cors=require('cors');
+const bodyParsel = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
-
 app.use(cors());
-// app.options('*',cors())
+//  app.options('*', cors());
 
 app.use(bodyParsel.json());
 
+// Import and use routes
+const categoryRoutes = require('./routes/category');
+app.use('/api/categories', categoryRoutes);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running http://localhost:${process.env.PORT}
-        `);
-})
+// Database
+mongoose.connect(process.env.CONNECTION_STRING)
+    .then(() => {
+        console.log('Database connection ready...');
+        app.listen(process.env.PORT, () => {
+            console.log(`server is running http://localhost:${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
