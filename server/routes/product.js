2a9b46ce8451f298fd39fae0a277d185ebd11002
Koushik-KS <kawshikks8@gmail.com>
2026-03-31 20:22:34 +0530

    commit

---
+++ server/routes/product.js
@@ -0,0 +1,37 @@
+
+const { Category } = require('../models/category');
+const { Product } = require('../models/products.js');
+const express = require('express');
+const router = express.Router();
+
+
+// GET
+router.get(`/`, async (req, res) => {
+  const productList = await Product.find().populate('category') ;
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productList);
+});
+
+router.post('/create', async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if (!category){
+
+     return res.status(404).send('Invalid Category');
+    }
+    let product = new Product(req.body);
+    product = await product.save();
+    if (!product) {
+        return res.status(500).json({
+           error:err,
+        success: false
+        })
+    }
+    res.status(201).json(product);
+});
+
+
+module.exports = router;
