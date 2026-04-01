89f2db39f475c42a6b48935bb079f138b97e8cfc
Koushik-KS <kawshikks8@gmail.com>
2026-04-01 22:19:24 +0530

    commit

--- server/routes/product.js
+++ server/routes/product.js
@@ -78,6 +78,8 @@ router.post('/create', async (req, res) => {
     res.status(201).json(product);
 });
 
+
+//get by id
 router.get('/:id', async (req, res) => {
   
     const product = await Product.findById(req.params.id);
@@ -106,6 +108,69 @@ router.delete('/:id', async (req, res) => {
     message: "the product is deleted!",
     status: true
   })
+})
+
+
+// ===================== PUT =====================
+router.put('/:id', async (req, res) => {
+
+
+  const limit = pLimit(2);
+    
+        const imagesToUpload = req.body.images.map((image) => {
+          return limit(async () => {
+            const result = await cloudinary.uploader.upload(image);
+            return result;
+          });
+        });
+    
+        const uploadStatus = await Promise.all(imagesToUpload);
+    
+        const imgurl = uploadStatus.map((item) => {
+          return item.secure_url
+        });
+    
+        if (!uploadStatus) {
+          return res.status(500).json({
+            error: "images cannot upload",
+            success: false
+          });
+        }
+
+   
+
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+       
+        name: req.body.name,
+        description: req.body.description,
+        images: imgurl,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
+ },
+   
+    { new: true }
+  );
+
+  if (!product) {
+    return res.status(404).json({
+      message: 'The product cannot be updated',
+      status: false
+    });
+  }
+
+ res.status(200).json({
+    message: 'The product is updated',
+    status: true,
+ 
+});
 })
 
 module.exports = router;
