8ae79e9513b8493f5b08c5c46ab29f8ef8d93189
Koushik-KS <kawshikks8@gmail.com>
2026-04-03 22:04:26 +0530

    commit

--- dashboard-reactjs-frontend/src/components/Header/index.js
+++ dashboard-reactjs-frontend/src/components/Header/index.js
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 import logo from '../../assets/images/logo.jpg';
 import Button from '@mui/material/Button';
 import { MdMenuOpen } from "react-icons/md";
+import SearchBox from "../SearchBox";
 
 const Header = () => {
     return (
         <header className="d-flex align-items-center">
             <div className="container-fluid w-100">
-                <div className="row d-flex align-items-center">
+                <div className="row d-flex align-items-center w-100">
 
                     {/* Logo Section */}
-                    <div className="col-sm-4 part1">
+                    <div className="col-sm-2 part1">
                         <Link to="/" className="d-flex align-items-center logo">
                             <img src={logo} alt="Store Logo" />
                             <span className="ml-2">STORE</span>
@@ -18,10 +19,11 @@ const Header = () => {
                     </div>
 
                     {/* Menu Button Section */}
-                    <div className="col-sm-3 d-flex align-items-center part2 pl-2">
-                        <Button className="rounded-circle mr-2">
+                    <div className="col-sm-3 d-flex align-items-center part2 pl-4">
+                        <Button className="rounded-circle mr-3">
                             <MdMenuOpen />
                         </Button>
+                        <SearchBox/>
                     </div>
 
                 </div>
