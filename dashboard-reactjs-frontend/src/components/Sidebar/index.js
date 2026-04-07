ae743d545883cd073170317287da5a3224bdad3f
Koushik-KS <kawshikks8@gmail.com>
2026-04-07 22:07:54 +0530

    commit

--- dashboard-reactjs-frontend/src/components/Sidebar/index.js
+++ dashboard-reactjs-frontend/src/components/Sidebar/index.js
@@ -9,7 +9,18 @@ import { MdMessage } from "react-icons/md";
 import { FaBell } from "react-icons/fa";
 import { IoSettings } from "react-icons/io5";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { AiOutlineLogout } from "react-icons/ai";
 const Sidebar =()=>{
+
+    const [activeTab,setActiveTab]=useState(0);
+     const [isToggleSubmenu, setIsToggleSubmenu]=useState(false);
+
+    const isOpenSubmenu=(index)=>{
+        setActiveTab(index);
+        setIsToggleSubmenu(!isToggleSubmenu);
+      
+    }
     return(
         <>
         <div className="sidebar">
@@ -17,7 +28,7 @@ const Sidebar =()=>{
                 <li>
                     <Link to="/">
                     
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===0 ? 'active' : ''}`}onClick={()=>isOpenSubmenu(0)}>
                     <span className='icon'><RiDashboardFill /></span>Dashboard 
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -25,23 +36,26 @@ const Sidebar =()=>{
                     </li>
 
                     <li>
-                        
-                        <Button className='w-100'>
+                              
+                        <Button className={`w-100 ${activeTab===1 && isToggleSubmenu===true ?
+                             'active' : ''}`} onClick={()=>isOpenSubmenu(1)}>
                     <span className='icon'><FaProductHunt /></span>Products 
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
-                    <ul>
+                    <div className={`submenuWrapper ${activeTab === 1 && isToggleSubmenu===true ? 'colapse' : 'colapsed'}`}>
+                    <ul className='submenu'>
                         <li><Link to="#">Product List</Link></li>
                         <li><Link to="#">Product View</Link></li>
                         <li><Link to="#">Product Upload</Link></li>
                     </ul>
+                    </div>
                    
                     </li>
 
                       <li>
                          <Link to="/">
                         
-                        <Button className='w-100'>
+                       <Button className={`w-100 ${activeTab===2 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(2)}>
                     <span className='icon'><IoCartOutline /></span>Orders
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -50,7 +64,7 @@ const Sidebar =()=>{
 
                      <li>
                          <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===3 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(3)}>
                     <span className='icon'><MdMessage /></span>Messages
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -59,7 +73,7 @@ const Sidebar =()=>{
 
                     <li>
                          <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===4 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(4)}>
                     <span className='icon'><FaBell /></span>Notifications
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -68,7 +82,7 @@ const Sidebar =()=>{
 
                     <li>
                        <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===5 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(5)}>
                     <span className='icon'><IoSettings /></span>Settings
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -77,7 +91,7 @@ const Sidebar =()=>{
 
                      <li>
                         <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===6 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(6)}>
                     <span className='icon'><RiDashboardFill /></span>Dashboard 
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -86,7 +100,7 @@ const Sidebar =()=>{
 
                     <li>
                         <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===7 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(7)}>
                     <span className='icon'><FaProductHunt /></span>Products 
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -95,7 +109,7 @@ const Sidebar =()=>{
 
                       <li>
                          <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===8 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(8)}>
                     <span className='icon'><IoCartOutline /></span>Orders
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -104,7 +118,7 @@ const Sidebar =()=>{
 
                      <li>
                          <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===9 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(9)}>
                     <span className='icon'><MdMessage /></span>Messages
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -113,7 +127,7 @@ const Sidebar =()=>{
 
                     <li>
                          <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===10 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(10)}>
                     <span className='icon'><FaBell /></span>Notifications
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
@@ -122,13 +136,20 @@ const Sidebar =()=>{
 
                     <li>
                         <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===11 ? 'active' : ''}`} onClick={()=>isOpenSubmenu(11)}>
                     <span className='icon'><IoSettings /></span>Settings
                     <span className='arrow'><IoIosArrowForward /></span>
                     </Button>
                     </Link>
                     </li>
             </ul>
+            <br/>
+            <div className='logoutWrapper'>
+                <div className='logoutBox'>
+                  <Button variant="contained"><AiOutlineLogout />Logout</Button>
+                  </div>
+
+            </div>
             </div>
         
         </>
