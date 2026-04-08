59b925630a3277e41f679537819f5127e1c62528
Koushik-KS <kawshikks8@gmail.com>
2026-04-08 20:32:00 +0530

    commit

---
+++ dashboard-reactjs-frontend/src/pages/Dashboard/components/dashboardBox.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+const DashboardBox = (props) => {
+
+   
+
+    return(
+      
+        <div className="dashboardBox" style={{
+    backgroundImage: `linear-gradient(to right, ${props.color?.[0]} , ${props.color?.[1]})`
+  }}>
+                    
+     </div>
+       
+    )
+}
+export default DashboardBox;
