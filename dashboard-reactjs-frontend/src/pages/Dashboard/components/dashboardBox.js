3d5e3129193b33a01e095939dbeaa0c403a7951a
Koushik-KS <kawshikks8@gmail.com>
2026-04-08 22:36:10 +0530

    commit

--- dashboard-reactjs-frontend/src/pages/Dashboard/components/dashboardBox.js
+++ dashboard-reactjs-frontend/src/pages/Dashboard/components/dashboardBox.js
@@ -7,8 +7,20 @@ const DashboardBox = (props) => {
     return(
       
         <div className="dashboardBox" style={{
-    backgroundImage: `linear-gradient(to right, ${props.color?.[0]} , ${props.color?.[1]})`
+    backgroundImage:
+     `linear-gradient(to right, ${props.color?.[0]} , ${props.color?.[1]})`
   }}>
+
+    <div className="d-flex w-100">
+        <div className="col1">
+        <h4 className="text-white">Total Users</h4>
+        <span className="text-white">277</span>
+        </div>
+        <div className="ml-auto">
+            <span className="icon"></span>
+        </div>
+    </div>
+
                     
      </div>
        
