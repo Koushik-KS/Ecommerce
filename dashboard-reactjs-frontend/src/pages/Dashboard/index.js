bd3688d4e0778646a5a4d605a9953801983f853f
Koushik-KS <kawshikks8@gmail.com>
2026-04-08 20:32:00 +0530

    commit

--- dashboard-reactjs-frontend/src/pages/Dashboard/index.js
+++ dashboard-reactjs-frontend/src/pages/Dashboard/index.js
@@ -1,29 +1,24 @@
+import DashboardBox from "./components/dashboardBox";
 
 
 const Dashboard = () => {
     return(
         <>
         <div className="right-content w-100">
-            <div className="row">
-                <div className="col-md-9">
+            <div className="row dashboardBoxWrapperRow">
+                <div className="col-md-8">
                     <div className="dashboardBoxWrapper d-flex">
                 
-                <div className="dashboardBox">
-                    
+              <DashboardBox color={["#1da256","#48d483"]}/> 
+                <DashboardBox color={["#c012e2","#eb64fe"]}/>
+                <DashboardBox color={["#2c78e5","#60aff5"]}/>
+                <DashboardBox color={["#e1950e","#f3cd29"]}/>
+                   
                 </div>
+                </div> 
 
-                  <div className="dashboardBox">
-                    
-                </div>
-
-                 <div className="dashboardBox">
-                    
-                </div>
-
-                 <div className="dashboardBox">
-                    
-                </div>
-                </div>
+                <div className="col-md-4 pl-0">
+                    <div className="box"></div>
                 </div>
 
 
