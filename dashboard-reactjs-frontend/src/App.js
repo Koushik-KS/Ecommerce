94f5dbd401ca3241213bf56da3c1fd3800aa8ab9
Koushik-KS <kawshikks8@gmail.com>
2026-04-15 22:39:30 +0530

    commit

--- dashboard-reactjs-frontend/src/App.js
+++ dashboard-reactjs-frontend/src/App.js
@@ -17,25 +17,36 @@ function App() {
   
   const [isToggleSidebar, setIsToggleSidebar]=useState(false);
   const [isLogin, setIsLogin]= useState(false);
-  const [isLogin, setIsLogin]= useState(false);
+  const [isHideSidebarAndHeader, setisHideSidebarAndHeader]= useState(false);
   
 
 const values={
   isToggleSidebar,
   setIsToggleSidebar,
   isLogin,
-   setIsLogin
+   setIsLogin,
+   isHideSidebarAndHeader, 
+   setisHideSidebarAndHeader
 }
 
   return (
    <BrowserRouter>
    <MyContext.Provider value={values}>
-   <Header/>
+   {
+     isHideSidebarAndHeader!==true &&
+    <Header/>
+   }
    <div className='main d-flex'>
-    <div className={`sidebarWrapper ${isToggleSidebar===true ? 'toggle' : ''}`}>
+    {
+      isHideSidebarAndHeader!==true &&
+      <div className={`sidebarWrapper ${isToggleSidebar===true ? 'toggle' : ''}`}>
       <Sidebar/>
     </div>
-    <div className={`content ${isToggleSidebar===true ? 'toggle' : ''}`}>
+
+    }
+    
+    <div className={`content ${isHideSidebarAndHeader===true && 'full'}
+     ${isToggleSidebar===true ? 'toggle' : ''}`}>
       <Routes>
     <Route path="/" exact={true} element={<Dashboard/>} />
     <Route path="/dashboard" exact={true} element={<Dashboard/>} />
