9bda672176cb303006dd566f58cea6e2d66ebed3
Koushik-KS <kawshikks8@gmail.com>
2026-04-27 19:08:15 +0530

    commit

--- dashboard-reactjs-frontend/src/pages/Dashboard/index.js
+++ dashboard-reactjs-frontend/src/pages/Dashboard/index.js
@@ -628,4 +628,4 @@ const Dashboard=()=>{
 
 
 }
-export default Dashboard;
+export default  Dashboard;
--- dashboard-reactjs-frontend/src/pages/ProductDetails/index.js
+++ dashboard-reactjs-frontend/src/pages/ProductDetails/index.js
@@ -1,10 +1,35 @@
+// // breadcrumb code
+// const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+//   const backgroundColor =
+//     theme.palette.mode === 'light'
+//       ? theme.palette.grey[100]
+//       : theme.palette.grey[800];
 
-const ProductDetails=()=>{
-    return(
-        <>
-        </>
-    )
+//   return {
+//     backgroundColor,
+//     height: theme.spacing(3),
+//     color: theme.palette.text.primary,
+//     fontWeight: theme.typography.fontWeightRegular,
+
+//     '&:hover, &:focus': {
+//       backgroundColor: emphasize(backgroundColor, 0.06),
+//     },
+
+//     '&:active': {
+//       boxShadow: theme.shadows[1],
+//       backgroundColor: emphasize(backgroundColor, 0.12),
+//     },
+//   };
+// });
+// const ProductDetails=()=>{
+//     return(
+//         <>
+//         <div className="right-content w-100">
+
+//         </div>
+//         </>
+//     )
 
 
-}
-export default ProductDetails;
+// }
+// export default ProductDetails;
