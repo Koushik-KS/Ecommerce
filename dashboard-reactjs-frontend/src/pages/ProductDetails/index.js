fa985efb19d4a7489c052a397990fb6eaffcc997
Koushik-KS <kawshikks8@gmail.com>
2026-05-14 23:15:00 +0530

    commit

--- dashboard-reactjs-frontend/src/pages/ProductDetails/index.js
+++ dashboard-reactjs-frontend/src/pages/ProductDetails/index.js
@@ -13,7 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { MdBrandingWatermark } from "react-icons/md";
 import { BiSolidCategory } from "react-icons/bi";
 import UserAvatarImgComponent from '../../components/userAvatarImg';
-
+import Rating from '@mui/material/Rating';
 
 // breadcrumb style
 const StyledBreadcrumb = styled(Chip)(({ theme }) => {
@@ -423,16 +423,16 @@ Easy to maintain and suitable for all seasons, it is a must-have addition to eve
   <div className='reviewsRow'>
     <div className='row'>
       <div className='col-sm-7'>
-        <div className='userInfo d-flex'>
-          <UserAvatarImgComponent img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTx3SRpQ8G8mKf3SUHnnn5mzgz7jx2WnePnA&s"
-          lg={true}/>
- <div className='info'>
+        <div className='userInfo d-flex align-items-center mb-3'>
+          <UserAvatarImgComponent img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTx3SRpQ8G8mKf3SUHnnn5mzgz7jx2WnePnA&s" lg={true}/>
+ <div className='info pl-3'>
           <h6>Koushik Shetty</h6>
           <span>25 minutes ago!</span>
         </div>
         </div>
-       
+        <Rating name="read-only" value={4.5}  precision={0.5}  readOnly />
       </div>
+
     </div>
 
   </div>
