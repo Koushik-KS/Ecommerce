c3dd9cc66525e60644dda349a3e66eafcee73312
Koushik-KS <kawshikks8@gmail.com>
2026-05-20 20:26:26 +0530

    commit

--- dashboard-reactjs-frontend/src/pages/ProductUpload/index.js
+++ dashboard-reactjs-frontend/src/pages/ProductUpload/index.js
@@ -1,12 +1,8 @@
 
 const ProductUpload = () => {
     return(
-      <>
-      <div>
-        <h1>Product Upload</h1>
-        <p>Welcome to the Product Upload page!</p>
-      </div>
-      </>
+      
+      <></>
     )
     
 }
