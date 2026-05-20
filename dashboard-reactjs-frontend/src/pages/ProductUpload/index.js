887e86c844496a141aa29b784f06c4584e8b3a3e
Koushik-KS <kawshikks8@gmail.com>
2026-05-20 23:39:03 +0530

    commit

--- dashboard-reactjs-frontend/src/pages/ProductUpload/index.js
+++ dashboard-reactjs-frontend/src/pages/ProductUpload/index.js
@@ -1,9 +1,78 @@
+import React from "react";
+
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import HomeIcon from '@mui/icons-material/Home';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Chip from '@mui/material/Chip';
+
+import { styled, emphasize } from '@mui/material/styles';
+
+const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+  const backgroundColor =
+    theme.palette.mode === 'light'
+      ? theme.palette.grey[100]
+      : theme.palette.grey[800];
+
+  return {
+    backgroundColor,
+    height: theme.spacing(3),
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightRegular,
+
+    '&:hover, &:focus': {
+      backgroundColor: emphasize(backgroundColor, 0.06),
+    },
+
+    '&:active': {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(backgroundColor, 0.12),
+    },
+  };
+});
 
 const ProductUpload = () => {
-    return(
-      
-      <></>
-    )
-    
-}
+  return (
+    <div className="right-content">
+
+      <div className="card header-row">
+
+        {/* LEFT SIDE */}
+        <h5 className="title">Product Upload</h5>
+
+        {/* RIGHT SIDE */}
+        <div className="breadcrumb-wrapper">
+
+          <Breadcrumbs aria-label="breadcrumb">
+
+            <StyledBreadcrumb
+              component="a"
+              href="#"
+              label="Dashboard"
+              icon={<HomeIcon fontSize="small" />}
+            />
+
+            <StyledBreadcrumb
+              label="Products"
+              href="#"
+              deleteIcon={<ExpandMoreIcon />}
+             />
+
+                <StyledBreadcrumb
+              label="Product Upload"
+              href="#"
+              deleteIcon={<ExpandMoreIcon />}
+             />
+
+            
+
+          </Breadcrumbs>
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
 export default ProductUpload;
