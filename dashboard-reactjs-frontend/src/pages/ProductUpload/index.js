5dd66bfecad072ab851031491f07050e44657d9a
Koushik-KS <kawshikks8@gmail.com>
2026-05-20 20:29:59 +0530

    comiit

--- dashboard-reactjs-frontend/src/pages/ProductUpload/index.js
+++ dashboard-reactjs-frontend/src/pages/ProductUpload/index.js
@@ -2,10 +2,54 @@
 const ProductUpload = () => {
     return(
       
-      <><div>
-        <h1>Product Upload</h1>
-        <p>Welcome to the Product Upload page!</p>
-      </div></>
+       <div className="row cardFilters mt-3">
+                    <div className="col-md-3">
+                    <h4>SHOW BY</h4>
+                     <FormControl  size="small" className="w-100">
+                     <Select
+              value={showBy}
+              onChange={(e)=>setshowBy(e.target.value)}
+              displayEmpty
+              inputProps={{ 'aria-label': 'Without label' }} 
+              labelId="demo-select-small-label"
+              className="w-100"
+            
+              >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+             </FormControl>
+                    </div>
+    
+                    <div className="col-md-3">
+                    <h4>CATEGORY BY</h4>
+                     <FormControl  size="small" className="w-100">
+                     <Select
+              value={showBysetCat}
+              onChange={(e)=>setCatBy(e.target.value)}
+              displayEmpty
+              inputProps={{ 'aria-label': 'Without label' }} 
+              labelId="demo-select-small-label"
+              className="w-100"
+            
+              >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+              </FormControl>
+                    </div>
+    
+    
+    
+                  </div>
     )
     
 }
